d7b319f1a7187cb4a049d60c4477cb95efeb9b74
icyJoseph <sephxd1234@gmail.com>
2018-10-28 23:03:40 +0100

    feat: allow users to share geolocation

--- public/js/components/Message.js
+++ public/js/components/Message.js
@@ -1,14 +1,20 @@
 const { Button, Form, Label, Input } = window["Reactstrap"];
 
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
 class Message extends React.Component {
   state = {
     value: "",
-    success: ""
+    notice: ""
   };
 
   handleChange = e => {
     e.preventDefault();
-    return this.setState({ value: e.target.value, success: "" });
+    return this.setState({ value: e.target.value, notice: "" });
   };
 
   submit = e => {
@@ -20,17 +26,39 @@ class Message extends React.Component {
         from: "Browser",
         text: this.state.value
       },
-      ({ msg }) => {
-        // currently ignoring the message
-        return this.clearWith(msg);
-      }
+      this.ack // acknowledgement from server
     );
   };
 
-  clearWith = msg => this.setState({ value: "", success: msg });
+  getLocation = () => {
+    if (!navigator.geolocation) {
+      return this.setState({ notice: "Geolocation not supported" });
+    }
+    return navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) =>
+        socket.emit(
+          "sharingGeolocation",
+          {
+            from: "Browser",
+            latitude,
+            longitude
+          },
+          this.ack // acknowledgement from server
+        ),
+      () => this.setState({ notice: "Geolocation not allowed" }),
+      options
+    );
+  };
+
+  ack = ({ msg }) => {
+    // currently ignoring the message
+    return this.clearWith(msg);
+  };
+
+  clearWith = msg => this.setState({ value: "", notice: msg });
 
   render() {
-    const { success } = this.state;
+    const { notice } = this.state;
     return (
       <div
         style={{
@@ -42,18 +70,26 @@ class Message extends React.Component {
         }}
       >
         <Label for="exampleText">
-          {success ? this.state.success : "Write something!"}
+          {notice ? this.state.notice : "Write something!"}
         </Label>
-        <Form onSubmit={this.submit} style={{ width: "100%", display: "flex" }}>
-          <Input
-            name="message"
-            type="text"
-            placeholder="Message"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-          <Button>Send</Button>
-        </Form>
+        <div style={{ width: "100%", display: "flex" }}>
+          <Form
+            onSubmit={this.submit}
+            style={{ width: "100%", display: "flex" }}
+          >
+            <Input
+              name="message"
+              type="text"
+              placeholder="Message"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+            <Button style={{ margin: "0 5px" }}>Send</Button>
+          </Form>
+          <Button style={{ margin: "0 5px" }} onClick={this.getLocation}>
+            Location
+          </Button>
+        </div>
       </div>
     );
   }
