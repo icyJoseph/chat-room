15fb58ec11f48669cb9a0af66b1b8b51d6c9e8b9
icyJoseph <sephxd1234@gmail.com>
2018-10-28 23:02:13 +0100

    chore: add message generator for location sharing

--- server/utils/message.js
+++ server/utils/message.js
@@ -4,4 +4,12 @@ const generateMessage = (from, text) => ({
   createdAt: new Date().getTime()
 });
 
-module.exports = { generateMessage };
+const generateMessageFromGeolocation = (from, lat, long) => ({
+  lat,
+  long,
+  from,
+  url: `https://www.google.com/maps/?q=${lat},${long}`,
+  createdAt: new Date().getTime()
+});
+
+module.exports = { generateMessage, generateMessageFromGeolocation };
