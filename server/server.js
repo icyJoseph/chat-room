1a96b48c60ff47c737901fc06837b437961dabd0
icyJoseph <sephxd1234@gmail.com>
2018-10-28 15:18:50 +0100

    refactor: http takes controls of express

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
 
-const app = express();
 const port = process.env.PORT || 1337;
-
 // path to front end files
 const publicPath = path.join(__dirname, "../public");
 
+const app = express();
+
+const server = http.createServer(app);
+
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Running in port ${port}`);
 });
