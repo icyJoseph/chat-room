9a7a186d2905bcfa90736c585d7fe05b0a88d106
icyJoseph <sephxd1234@gmail.com>
2018-10-29 13:20:35 +0100

    test: url for location message

--- server/utils/tests/index.test.js
+++ server/utils/tests/index.test.js
@@ -1,5 +1,8 @@
 const expect = require("expect");
-const { generateMessage } = require("../message");
+const {
+  generateMessage,
+  generateMessageFromGeolocation
+} = require("../message");
 
 describe("utils", () => {
   it("generates message", () => {
@@ -7,6 +10,17 @@ describe("utils", () => {
     expect(result).toMatch({
       from: "user",
       text: "text used"
+    });
+    expect(result.createdAt).toBeA("number");
+  });
+
+  it("generates url with coordinates", () => {
+    const result = generateMessageFromGeolocation("user", 12, 10);
+    expect(result).toMatch({
+      from: "user",
+      lat: 12,
+      long: 10,
+      url: "https://www.google.com/maps/?q=12,10"
     });
     expect(result.createdAt).toBeA("number");
   });
