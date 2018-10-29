c717c8f3ce121e058ec68e464fa1eab23e0b955e
icyJoseph <sephxd1234@gmail.com>
2018-10-29 15:10:37 +0100

    feat: evaluate connected status and pass it to Welcome

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,35 @@
 // returns the socket management
 const socket = io();
 
-socket.on("connect", function() {
-  console.log("Gained connection to server");
-});
+function setConnection(payload) {
+  console.log("Connection status: ", payload);
+  return this.setConnection(payload);
+}
 
-socket.on("disconnect", function() {
-  console.log("Connection dropped :(");
-});
+class App extends React.Component {
+  state = {
+    connected: false
+  };
 
-const App = () => (
-  <React.Fragment>
-    <Welcome />
-    <MessageList>
-      <MessageInput />
-    </MessageList>
-  </React.Fragment>
-);
+  componentDidMount() {
+    socket.on("connect", setConnection.bind(this, true));
+
+    socket.on("disconnect", setConnection.bind(this, false));
+  }
+
+  setConnection = payload => this.setState({ connected: payload });
+
+  render() {
+    const { connected } = this.state;
+    return (
+      <React.Fragment>
+        <Welcome connected={connected} />
+        <MessageList>
+          <MessageInput />
+        </MessageList>
+      </React.Fragment>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
