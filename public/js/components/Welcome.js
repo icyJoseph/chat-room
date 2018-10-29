3a1b32ef188e8d50e07be4ef1d92b81de115f87d
icyJoseph <sephxd1234@gmail.com>
2018-10-29 15:11:00 +0100

    feat: display connection status

--- public/js/components/Welcome.js
+++ public/js/components/Welcome.js
@@ -1,10 +1,13 @@
-const { Jumbotron } = window["Reactstrap"];
+const { Badge, Jumbotron } = window["Reactstrap"];
 
-const Welcome = () => {
+const Welcome = ({ connected }) => {
   return (
     <div className="flex-height">
       <Jumbotron>
         <h1 className="display-3">Hello, world!</h1>
+        <Badge color={connected ? "success" : "danger"}>
+          Connection: {connected ? "OK" : "Bad"}
+        </Badge>
         <p className="lead">This is a simple chat app.</p>
         <hr className="my-2" />
         <p>It uses SocketIO to bring people together</p>
